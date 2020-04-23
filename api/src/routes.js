61fd1a1f91b0e378c531d7e727dd598fd922eb59
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-23 14:34:08 -0300

    Delete and getall routes

--- api/src/routes.js
+++ api/src/routes.js
@@ -4,6 +4,22 @@ const multer = require('multer');
 const multerConfig = require('./config/multer');
 const Post = require('./models/Post');
 
+routes.get('/posts', async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const post = await Post.findById(id);
+
+  await post.remove();
+
+  return res.send();
+});
+
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
   const { originalname: name, size, key, location: url = '' } = req.file;
 
